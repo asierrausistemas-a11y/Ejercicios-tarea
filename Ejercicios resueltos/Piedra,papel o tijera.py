8c6ba6840b158c908b5b0dd4ca2b1ab54062a4a1
Andres Sierra <asierra_usistemas@intep.edu.co>
2026-04-27 15:46:28 -0500

    Actializacio ejercicios resueltos

---
+++ Ejercicios resueltos/Piedra,papel o tijera.py
@@ -0,0 +1,41 @@
+"""""
+1. Descripción de problema
+Se solicita desarrolar un programa interactivo en Python que permita a un usuario
+enfrentarse contra la computadora en el Clasico juego de Piedra, papel o tijera.
+El programa debe ser capaz de procesar la entrada del usuario, generar una respuesta 
+aleatoria y determinar un ganador basandose en las reglas tradicionales.
+
+2. Requerimentos Técnicos
+El algoritmo debe estrucuturarse de la siguiente manera:
+
+*interfaz Visual: Mostrar un encabezado decorativo utilizando métodos de cadena como .center()
+y multiplicacion de caracteres 
+
+*Entrada de datos: Solicitar al usuario su eleccion. El programa debe ser capaz de reconocer
+la entrada sin importar si se escribe en mayusculas o minusculas.
+
+*inteligencia Aleatoria: La computadora debe elegir una opcion de una lista predefinida de
+opciones (Piedra, Papel, Tijera)
+de forma aleatoria utilizando el modulo random.
+
+*Logica de comparación:
+Implementar las conidiciones necesarias para evaluar.
+
+1.Empate: Ambas elecciones son iguales.
+2.Victoria: El usuario vence a la PC
+(Piedra vence a Tijera, Tijera vence a Papel, Papel vence a Piedra)
+3.Derrota: La PC vence al usuario
+
+*Control de Flujo: El juego debe repetirse indefinidamente dentro de un bucle hasta que 
+el usuario decida escribir la palabra (Salir)
+
+
+
+Tarea para la casa, aparte del trabajo, Debe programar y subir al github.
+
+Debe programar una barra de carga que se ejecute durante 3 sefundos
+
+por medio de bucles imprime en la terminal las siguientes figuras geometricas 
+Cuadradp 
+Triangulo
+Rectangulo"""
