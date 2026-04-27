9aca839e8437e6796f010676f0d851d18ac30822
Andres Sierra <asierra_usistemas@intep.edu.co>
2026-04-27 17:30:41 -0500

    jejeje

--- Ejercicios resueltos/Piedra,papel o tijera.py
+++ Ejercicios resueltos/Piedra,papel o tijera.py
@@ -1,72 +1,34 @@
-"""""
+'''
+Reto de programación # 1: Piedra, Papel o Tijera
+
 1. Descripción de problema
-Se solicita desarrolar un programa interactivo en Python que permita a un usuario
-enfrentarse contra la computadora en el Clasico juego de Piedra, papel o tijera.
-El programa debe ser capaz de procesar la entrada del usuario, generar una respuesta 
-aleatoria y determinar un ganador basandose en las reglas tradicionales.
+Se solicita desarrollar un programa interactivo en Python que permita a un usuario enfrentarse contra la computadora en el 
+Clásico juego de Piedra, Papel o Tijera.
+El programa debe ser capaz de procesar la entrada del usuario, generar una respuesta aleatoria y determinar un ganador 
+basándose en las reglas tradicionales.
 
-2. Requerimentos Técnicos
-El algoritmo debe estrucuturarse de la siguiente manera:
+2. Requerimientos Técnicos
 
-*interfaz Visual: Mostrar un encabezado decorativo utilizando métodos de cadena como .center()
-y multiplicacion de caracteres 
+El algoritmo debe estructurarse de la siguiente manera:
 
-*Entrada de datos: Solicitar al usuario su eleccion. El programa debe ser capaz de reconocer
-la entrada sin importar si se escribe en mayusculas o minusculas.
+* Interfaz Visual: Mostrar un encabezado decorativo utilizando métodos de cadena como .center() y multiplicación de caracteres.
 
-*inteligencia Aleatoria: La computadora debe elegir una opcion de una lista predefinida de
-opciones (Piedra, Papel, Tijera)
-de forma aleatoria utilizando el modulo random.
+* Entrada de Datos: Solicitar al usuario su elección. El programa debe ser capaz de reconocer la entrada sin importar si 
+se escribe en mayúsculas o minúsculas.
 
-*Logica de comparación:
-Implementar las conidiciones necesarias para evaluar.
+* Inteligencia Aleatoria: La computadora debe elegir una opción de una lista predefinida de opciones (Piedra, Papel, Tijera) 
+de forma aleatoria utilizando el módulo random.
 
-1.Empate: Ambas elecciones son iguales.
-2.Victoria: El usuario vence a la PC
-(Piedra vence a Tijera, Tijera vence a Papel, Papel vence a Piedra)
-3.Derrota: La PC vence al usuario
+* Logica de comparación:
 
-*Control de Flujo: El juego debe repetirse indefinidamente dentro de un bucle hasta que 
-el usuario decida escribir la palabra (Salir)
-"""
+Implementar las condiciones necesarias para evaluar.
 
-import random, time 
+1. Empate: Ambas elecciones son iguales.
+2. Victoria: El usuario vence a la PC (Priedra vence a Tijera, Tijera vence a Papel, Papel vence a Piedra).
+3. Derrota: La PC vence al usuario.
 
-print("="*55)
-print("Bienvenido al clasico juego de piedra, papel o tijera")
-print("="*55)
+* Control de Flujo: El juego debe repetirse indefinidamente dentro de un blucle hasta que el usuario decida escribir 
+la palabra (Salir).
 
-opciones= ["piedra", "papel", "tijera"]
-
-
-input("para empezar el juego(presiona enter)")
-
-
-while True:
-    usuario = input("escoja: piedra, papel o tijera (o salir):").lower()
-
-    if usuario == "salir":
-        print("Fin del juego")
-        break
-
-    pc = random.choice(opciones)
-
-    print("Tú:", usuario)
-    print("PC:", pc)
-
-    if usuario == pc:
-        print("Empate")
-    elif usuario == "piedra" and pc == "tijera":
-        print("Ganaste")
-    elif usuario == "tijera" and pc == "papel":
-        print("Ganaste")
-    elif usuario == "papel" and pc == "piedra":
-        print("Ganaste")
-    else:
-        print("Perdiste")
-
-
-
-
-
-
+Solución del Ejercicio Propuesto    
+'''
