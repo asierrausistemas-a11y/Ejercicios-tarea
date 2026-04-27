5952ea92344557bb7f1575d39210b29cc9b054ef
Andres Sierra <asierra_usistemas@intep.edu.co>
2026-04-27 17:28:45 -0500

    resueltos

--- Ejercicios resueltos/Piedra,papel o tijera.py
+++ Ejercicios resueltos/Piedra,papel o tijera.py
@@ -28,14 +28,45 @@ Implementar las conidiciones necesarias para evaluar.
 
 *Control de Flujo: El juego debe repetirse indefinidamente dentro de un bucle hasta que 
 el usuario decida escribir la palabra (Salir)
+"""
+
+import random, time 
+
+print("="*55)
+print("Bienvenido al clasico juego de piedra, papel o tijera")
+print("="*55)
+
+opciones= ["piedra", "papel", "tijera"]
+
+
+input("para empezar el juego(presiona enter)")
+
+
+while True:
+    usuario = input("escoja: piedra, papel o tijera (o salir):").lower()
+
+    if usuario == "salir":
+        print("Fin del juego")
+        break
+
+    pc = random.choice(opciones)
+
+    print("Tú:", usuario)
+    print("PC:", pc)
+
+    if usuario == pc:
+        print("Empate")
+    elif usuario == "piedra" and pc == "tijera":
+        print("Ganaste")
+    elif usuario == "tijera" and pc == "papel":
+        print("Ganaste")
+    elif usuario == "papel" and pc == "piedra":
+        print("Ganaste")
+    else:
+        print("Perdiste")
 
 
 
-Tarea para la casa, aparte del trabajo, Debe programar y subir al github.
 
-Debe programar una barra de carga que se ejecute durante 3 sefundos
 
-por medio de bucles imprime en la terminal las siguientes figuras geometricas 
-Cuadradp 
-Triangulo
-Rectangulo"""
+
